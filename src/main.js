3281ec81dd89ec5c04668608af6793d3afc00316
Matheus Henrique <matheus.hsilvaa18@gmail.com>
2025-09-03 17:20:25 -0300

    feat: update deployment test count to 4 and include current time in response message

--- src/main.js
+++ src/main.js
@@ -7,7 +7,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.json({
     msg: "Continuous Deployment with Docker, Cloud Build, and Cloud Run",
-    deployment_test_count: 3
+    deployment_test_count: 4,
+    time: new Date()
   });
 });
 
