fc7731ac66e9b5743cfc5bc67630ab943b70df42
Matheus Henrique <matheus.hsilvaa18@gmail.com>
2025-09-03 17:11:52 -0300

    feat: update deployment test count in response message

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const app = express();
 app.get('/', (req, res) => {
   res.json({
     msg: "Continuous Deployment with Docker, Cloud Build, and Cloud Run",
-    deployment_test_count: 1
+    deployment_test_count: 2
   });
 });
 
